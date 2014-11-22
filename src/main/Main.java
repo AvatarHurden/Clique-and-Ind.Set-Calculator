69fdc9834454bde3ce58e73f6a509155f08c5a08
Arthur Vedana <agieselvedana@gmail.com>
2014-11-22 10:45:09 -0200

    Limpada classe Main e mudada para Java 1.7

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,62 +1,10 @@
 package main;
-import java.util.ArrayList;
-import java.util.List;
-
 import GUI.GraphFrame;
 
 
 public class Main {
 	
 	public static void main(String args[]) {
-		
-		Node node1 = new Node(1, 3, 9);
-		Node node2 = new Node(2, 4, 7);
-		Node node3 = new Node(3, 1, 9);
-		Node node4 = new Node(4, 6, 9);
-		Node node5 = new Node(5, 9);
-		Node node6 = new Node(6, 2, 8);
-		Node node7 = new Node(7, 9);
-		Node node8 = new Node(8, 2, 9);
-		Node node9 = new Node(9, 1, 3, 4, 5, 7, 8);
-		Node node10 = new Node(10);
-		Node node11 = new Node(11, 1, 3, 4, 5, 7, 8);
-		Node node12 = new Node(12, 1, 3, 4, 5, 7, 8);
-		
-		Graph graph = new Graph();
-		graph.addNode(node1);
-		graph.addNode(node2);
-		graph.addNode(node3);
-		graph.addNode(node4);
-		graph.addNode(node5);
-		graph.addNode(node6);
-		graph.addNode(node7);
-		graph.addNode(node8);
-		graph.addNode(node9);
-		graph.addNode(node10);
-		
-		List<Node> order = new ArrayList<Node>();
-		order.add(node1);
-		order.add(node2);
-		order.add(node3);
-		order.add(node4);
-		order.add(node5);
-		order.add(node6);
-		order.add(node7);
-		order.add(node8);
-		order.add(node9);
-		order.add(node10);
-		
-		System.out.println("Starting");
-		
-		//System.out.println(graph.getMaximalIS());
-		
-		//System.out.println(graph.getIndependentSet(order.toArray(new Node [order.size()])));
-		
-//		ArrayList<Node[]> permutaions = (per.permutations(order.toArray(new Node[]{})));
-		
-		System.out.println("Permuted");
-		
-		
 		new GraphFrame();
 	}
 	
